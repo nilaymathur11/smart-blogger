e83f2ae914ac34d5413d70d97261a878867b92cc
nilaymathur11 <nilaym2k2@gmail.com>
2023-11-13 17:05:51 +0530

    commit

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -20,32 +20,24 @@ export default async function BlogPage({ params }) {
         console.log(error);
     }
     return (
-        <>
-            {
-                blogData ?
-                    <div className='md:px-[250px] px-[25px] md:mb-[100px] mb-[50px] md:mt-[30px] mt-[50px]'>
-                        <div className='grid md:grid-cols-2 grid-cols-1 md:gap-[50px] gap-[25px] items-center'>
-                            <div>
-                                <Image src={`/uploads/${blogData.blog_image}`} width={100} height={100} className='w-[100%] md:h-[400px] h-[250px] rounded-md shadow-lg object-cover object-top' alt='blogImg' />
-                            </div>
-                            <div>
-                                <div className='md:text-[50px] text-[32px] font-bold md:leading-[60px] leading-[40px]'>{blogData.blog_title}</div>
-                                <div className='font-light mt-[25px]'>
-                                    by {blogData?.author_name}
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[100%] md:h-[400px] h-[250px] rounded-md my-[50px]'>
-                            <video controls loop muted className='w-[60%] md:h-[400px] h-[250px] rounded-md shadow-lg' src={`/uploads/${blogData.blog_video}`} />
-                        </div>
-                        <div className='text-[18px] leading-[40px]'>
-                            {blogData.blog_content}
-                        </div>
+        <div className='md:px-[250px] px-[25px] md:mb-[100px] mb-[50px] md:mt-[30px] mt-[50px]'>
+            <div className='grid md:grid-cols-2 grid-cols-1 md:gap-[50px] gap-[25px] items-center'>
+                <div>
+                    <Image src={`/uploads/${blogData.blog_image}`} width={100} height={100} className='w-[100%] md:h-[400px] h-[250px] rounded-md shadow-lg object-cover object-top' alt='blogImg' />
+                </div>
+                <div>
+                    <div className='md:text-[50px] text-[32px] font-bold md:leading-[60px] leading-[40px]'>{blogData.blog_title}</div>
+                    <div className='font-light mt-[25px]'>
+                        by {blogData?.author_name}
                     </div>
-                    :
-                    <div>Loading...</div>
-            }
-        </>
-
+                </div>
+            </div>
+            <div className='w-[100%] md:h-[400px] h-[250px] rounded-md my-[50px]'>
+                <video controls loop muted className='w-[60%] md:h-[400px] h-[250px] rounded-md shadow-lg' src={`/uploads/${blogData.blog_video}`} />
+            </div>
+            <div className='text-[18px] leading-[40px]'>
+                {blogData.blog_content}
+            </div>
+        </div>
     )
 }
